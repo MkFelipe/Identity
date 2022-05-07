ba2ba7c6522c615e5290a9ebdd698c4f718bd6aa
Felipe Miertschink <felipe@agilesoftware.com>
2022-05-07 18:55:28 -0300

    Repository done

--- src/Identity.Domain/Entitites/Interfaces/IEntity.cs
+++ src/Identity.Domain/Entitites/Interfaces/IEntity.cs
@@ -9,6 +9,9 @@ namespace Identity.Domain.Entities.interfaces
 	public interface IEntity
 	{
 		Guid EntityId { get; set; }
+		public bool IsDeleted { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
 	}
 }
 
